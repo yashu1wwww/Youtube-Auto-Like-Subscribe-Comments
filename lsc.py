b0bb22d23ab625219ac0b803879a541d7be48de0
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-11-19 15:05:04 +0530

    Update lsc.py

--- lsc.py
+++ lsc.py
@@ -1,4 +1,4 @@
-from selenium import webdriver 
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,17 +11,18 @@ import random
 commentsDict = ['good','amazing one','keep going','excellent','next video please','sub to your channel','shared to others','made my day','keep it up','sensational','rock it','challenge it','post video daily','work was amazing','needed more edit','edit was awesome',
 'what a video man','watched yesterday','your are genious','faster than light','your work needed success','new fan of you','keep rock dude','copy cat','link the video','listening','writing','reading','playing',] #replace with your words
 
-driver = webdriver.Chrome()          
-driver.get('https://stackoverflow.com/users/signup') #when using to login youtube through stackoverflow you must signup to stackoverflow and use that account here(off 2 factor authentication in google my account)
-time.sleep(1) 
-driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click() 
-driver.find_element_by_id('identifierId').send_keys("when123@gmail.com") #replace with your mail which used to signup in stackoverflow
-driver.find_element_by_id('identifierNext').click() 
-time.sleep(2) 
-driver.find_element_by_name('password').send_keys("pas2@#$%") #replace with your password which used to signup in stackoverflow
-driver.find_element_by_id('passwordNext').click() 
+email = 'allyoman\n'      #replace with your gmail(use which accounts dont have 2 factor authentication)
+password = 'pass232@12\n'  #replace with your password
+
+driver = uc.Chrome(use_subprocess=True)
+wait = WebDriverWait(driver,20)
+url = 'https://accounts.google.com/AddSession?continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den-GB%26next%3D%252F&hl=en-GB&passive=false&service=youtube&uilel=0'
+driver.get(url)
+
+wait.until(EC.visibility_of_element_located((By.NAME,'identifier'))).send_keys(email)
+#time.sleep(2)
+wait.until(EC.visibility_of_element_located((By.NAME,'Passwd'))).send_keys(password)
 time.sleep(2)
-driver.get('https://www.youtube.com/') 
 
 with open("urls.txt") as f:
     for url in f:
@@ -39,7 +40,7 @@ time.sleep(2)
 
 driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[1]/div/ytd-subscribe-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]').click()
 
-time.sleep(3)
+time.sleep(5)
 
 driver.execute_script("window.scrollTo(0, 600);")
 
@@ -404,4 +405,3 @@ send_comment_button = driver.find_element_by_id("submit-button").click()
 time.sleep(5)
 
 driver.close()
-
